f9c53eb1fcfae4b87110a510c72325b5778ce313
marinacor1 <marinaspam1234@gmail.com>
2016-03-29 12:02:45 -0600

    adding tests

--- test/models/payload_request_test.rb
+++ test/models/payload_request_test.rb
@@ -44,4 +44,145 @@ class PayloadRequestTest < Minitest::Test
                                   "ip":"63.29.38.211"
                                 }).valid?
   end
+
+  def test_payload_is_not_valid_if_url_is_missing 
+    refute PayloadRequest.create({
+                                  "requestedAt":"2013-02-16 21:38:28 -0700",
+                                  "respondedIn":37,
+                                  "referredBy":"http://jumpstartlab.com",
+                                  "requestType":"GET",
+                                  "eventName": "socialLogin",
+                                  "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+                                  "resolutionWidth":"1920",
+                                  "resolutionHeight":"1280",
+                                  "ip":"63.29.38.211"
+                                }).valid?
+  end
+
+  def test_payload_is_not_valid_if_requested_at_is_missing 
+    refute PayloadRequest.create({
+                                  "url":"http://jumpstartlab.com/blog",
+                                  "respondedIn":37,
+                                  "referredBy":"http://jumpstartlab.com",
+                                  "requestType":"GET",
+                                  "eventName": "socialLogin",
+                                  "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+                                  "resolutionWidth":"1920",
+                                  "resolutionHeight":"1280",
+                                  "ip":"63.29.38.211"
+                                }).valid?
+  end
+
+  def test_payload_is_not_valid_if_responded_in_is_missing 
+    refute PayloadRequest.create({
+                                  "url":"http://jumpstartlab.com/blog",
+                                  "requestedAt":"2013-02-16 21:38:28 -0700",
+                                  "referredBy":"http://jumpstartlab.com",
+                                  "requestType":"GET",
+                                  "eventName": "socialLogin",
+                                  "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+                                  "resolutionWidth":"1920",
+                                  "resolutionHeight":"1280",
+                                  "ip":"63.29.38.211"
+                                }).valid?
+  end
+
+  def test_payload_is_not_valid_if_referred_by_is_missing 
+    refute PayloadRequest.create({
+                                  "url":"http://jumpstartlab.com/blog",
+                                  "requestedAt":"2013-02-16 21:38:28 -0700",
+                                  "respondedIn":37,
+                                  "requestType":"GET",
+                                  "eventName": "socialLogin",
+                                  "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+                                  "resolutionWidth":"1920",
+                                  "resolutionHeight":"1280",
+                                  "ip":"63.29.38.211"
+                                }).valid?
+  end
+
+  def test_payload_is_not_valid_if_request_type_is_missing
+    refute PayloadRequest.create({
+                                  "url":"http://jumpstartlab.com/blog",
+                                  "requestedAt":"2013-02-16 21:38:28 -0700",
+                                  "respondedIn":37,
+                                  "referredBy":"http://jumpstartlab.com",
+                                  "eventName": "socialLogin",
+                                  "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+                                  "resolutionWidth":"1920",
+                                  "resolutionHeight":"1280",
+                                  "ip":"63.29.38.211"
+                                }).valid?
+  end
+
+  def test_payload_is_not_valid_if_event_name_is_missing
+    refute PayloadRequest.create({
+                                  "url":"http://jumpstartlab.com/blog",
+                                  "requestedAt":"2013-02-16 21:38:28 -0700",
+                                  "respondedIn":37,
+                                  "referredBy":"http://jumpstartlab.com",
+                                  "requestType":"GET",
+                                  "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+                                  "resolutionWidth":"1920",
+                                  "resolutionHeight":"1280",
+                                  "ip":"63.29.38.211"
+                                }).valid?
+  end
+
+  def test_payload_is_not_valid_if_user_agent_is_missing
+    refute PayloadRequest.create({
+                                  "url":"http://jumpstartlab.com/blog",
+                                  "requestedAt":"2013-02-16 21:38:28 -0700",
+                                  "respondedIn":37,
+                                  "referredBy":"http://jumpstartlab.com",
+                                  "requestType":"GET",
+                                  "eventName": "socialLogin",
+                                  "resolutionWidth":"1920",
+                                  "resolutionHeight":"1280",
+                                  "ip":"63.29.38.211"
+                                }).valid?
+  end
+
+  def test_payload_is_not_valid_if_res_width_missing
+    refute PayloadRequest.create({
+                                  "url":"http://jumpstartlab.com/blog",
+                                  "requestedAt":"2013-02-16 21:38:28 -0700",
+                                  "respondedIn":37,
+                                  "referredBy":"http://jumpstartlab.com",
+                                  "requestType":"GET",
+                                  "eventName": "socialLogin",
+                                  "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+                                  "resolutionHeight":"1280",
+                                  "ip":"63.29.38.211"
+                                }).valid?
+  end
+
+  def test_payload_is_not_valid_if_res_height_missing
+    refute PayloadRequest.create({
+                                  "url":"http://jumpstartlab.com/blog",
+                                  "requestedAt":"2013-02-16 21:38:28 -0700",
+                                  "respondedIn":37,
+                                  "referredBy":"http://jumpstartlab.com",
+                                  "requestType":"GET",
+                                  "eventName": "socialLogin",
+                                  "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+                                  "resolutionWidth":"1920",
+                                  "ip":"63.29.38.211"
+                                }).valid?
+  end
+
+  def test_payload_is_not_valid_if_ip_is_missing
+    refute PayloadRequest.create({
+                                  "url":"http://jumpstartlab.com/blog",
+                                  "requestedAt":"2013-02-16 21:38:28 -0700",
+                                  "respondedIn":37,
+                                  "referredBy":"http://jumpstartlab.com",
+                                  "requestType":"GET",
+                                  "eventName": "socialLogin",
+                                  "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+                                  "resolutionWidth":"1920",
+                                  "resolutionHeight":"1280",
+                                }).valid?
+  end
+
 end
