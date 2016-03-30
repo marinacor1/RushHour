62f2e877a16c55d5beac1856d55632318d0365fd
patrickwhardy <patrickwhardy@gmail.com>
2016-03-30 11:48:26 -0600

    uniqueness added to url

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,7 @@
-class Url < ActiveRecord::Base
+require_relative 'payload_request'
 
+class Url < ActiveRecord::Base
+  validates :address, uniqueness: true
   def self.sort_url_requests
     # verb_count is a hash with key = url, value = count of key
     url_count = self.select(:address).group(:address).having("count(*) > 0").count
@@ -10,5 +12,16 @@ class Url < ActiveRecord::Base
     end.reverse
   end
 
+  def self.max_response_time(url)
+    #find url_id
+    #find_by url_id in payload request
+    #sort by responded in
+    #return max
+    # url_id = self.find_by(address: url.address).id
+    # payloads = PayloadRequest.find_each(url_id: url.id) { |payload| payload.url_id == url.id}
+    # payloads.count
+    # payloads = PayloadRequest.find(:all, :conditions => ["SELECT * FROM"])
+    PayloadRequest.where(url_id: url.id)
+  end
 
 end
--- test/models/url_test.rb
+++ test/models/url_test.rb
@@ -1,18 +1,29 @@
 require_relative "../test_helper"
 
 class UrlTest < Minitest::Spec
-
   include TestHelpers
 
-  def test_can_list_urls_by_num_times_requested
+  def setup
     create_payloads
+  end
 
+  def test_duplicate_urls_are_eliminated
+    assert_equal 3, Url.all.count
+  end
+
+  def test_can_list_urls_by_num_times_requested
     url_list = Url.sort_url_requests
-
+    # require 'pry';binding.pry
     assert_equal "http://jumpstartlab.com/", url_list[0][0]
     assert_equal "http://turing.io/", url_list[1][0]
     assert_equal "http://yahoo.com/", url_list[2][0]
     assert_equal 3, url_list.count
+  end
+
+  def test_returns_max_response_time_by_url
+    skip
+    url = Url.find_by(address: "http://jumpstartlab.com/")
+    assert_equal 34, Url.max_response_time(url)
   end
 
 end
