d9886ba6d2f1e0046339a447cd5075913f2d137c
marinacor1 <marinaspam1234@gmail.com>
2016-03-29 22:08:36 -0600

    adding tests to deal with single

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,18 +12,46 @@ class UserTest < Minitest::Spec
     assert browser.any? { |b| b == "Chrome" }
     assert browser.any? { |b| b == "Safari" }
     assert browser.any? { |b| b == "Safari" }
+    assert_equal ["Chrome", "Chrome", "Safari", "Safari"], browser
   end
 
   def test_user_returns_breakdown_of_os
     create_payloads
 
-    browser = User.os_breakdown
+    os = User.os_breakdown
 
-    assert browser.any? { |b| b == "Macintosh" }
-    assert browser.any? { |b| b == "Macintosh" }
-    assert browser.any? { |b| b == "Linux" }
-    assert browser.any? { |b| b == "Windows" }
+    assert os.any? { |b| b == "Macintosh" }
+    assert os.any? { |b| b == "Macintosh" }
+    assert os.any? { |b| b == "Linux" }
+    assert os.any? { |b| b == "Windows" }
+    assert_equal ["Macintosh", "Macintosh", "Linux", "Windows"], os
+  end
 
+  def test_user_will_not_return_arbitrary_browser
+    create_payloads
+
+    browser = User.browser_breakdown
+
+    refute browser.any? { |b| b == "Charlie Brown" }
+  end
+
+  def test_user_will_return_one_browser_for_single_payload
+    PayloadRequest.create({ url_id: Url.create(address: "http://jumpstartlab.com/").id,
+                            requested_at: "2013-02-16 21:38:28 -0700",
+                            responded_in: 37,
+                            request_id: Request.create(referred_by:"http://jumpstartlab.com").id,
+                            request_type_id: RequestType.create(verb: "GET").id,
+                            event_name: "socialLogin",
+                            user_id: User.create(browser: UserAgent.parse("Mozilla/5.0 (Macintosh;) AppleWebKit/537.17 Safari/537.17").browser,
+                                                 os: UserAgent.parse("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17").platform
+                                                 ).id,
+                            display_id: Display.create(width: "1920", height: "1280").id,
+                            ip: "63.29.38.211"
+                          })
+
+    browser = User.browser_breakdown
+
+    assert_equal ["Safari"], browser
   end
 
 end
