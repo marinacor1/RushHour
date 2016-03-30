e14ad958cdf861c51b0bd60d6d8f13310c626c36
marinacor1 <marinaspam1234@gmail.com>
2016-03-30 17:00:26 -0600

    cleaning up

--- app/models/referrer.rb
+++ app/models/referrer.rb
@@ -3,7 +3,7 @@ class Referrer < ActiveRecord::Base
   has_many :urls, through: :payload_requests
   def self.list_referrers
     # referrer_count = self.select(:referred_by).group(:referred_by).count
-    # sorted = referrer_count.sort_by { |referrer, count| count }
+    # sortgsed = referrer_count.sort_by { |referrer, count| count }
     # sorted.reverse.map { |referrer, count| referrer }
 
     #Payload - hash with referrer id => # of times it occurs
--- app/models/request_type.rb
+++ app/models/request_type.rb
@@ -1,13 +1,12 @@
 class RequestType < ActiveRecord::Base
 
   def self.list_verbs
-    # verb_count is a hash with key = verb, value = count of key
-    verb_count = self.select(:verb).group(:verb).having("count(*) > 0").count
-    # sort_by the count + reverse returns list of verbs in descending order of count
-    # might need to do a better job of using sql here
-    verb_count.sort_by do |verb, count|
-      count
-    end.reverse.map { |verb, count| verb }
+
+
+    # verb_count = self.select(:verb).group(:verb).having("count(*) > 0").count
+    # verb_count.sort_by do |verb, count|
+    #   count
+    # end.reverse.map { |verb, count| verb }
   end
 
   def self.most_requested
