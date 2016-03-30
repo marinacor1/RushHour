97975479478395cea8ab90f89ec051e52948e639
Ashwin Rao <theonlyrao@gmail.com>
2016-03-29 21:23:40 -0600

    url method for sorted list of requested urls working

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,14 @@
 class Url < ActiveRecord::Base
+
+  def self.sort_url_requests
+    # make a count of how many times each url is requested
+
+    # then return a list that is sorted by the count
+    url_count = self.select(:address).group(:address).having("count(*) > 0").count
+    url_count.sort_by do |k, v|
+      v
+    end.reverse
+  end
+
+
 end
---
+++ test/models/url_test.rb
@@ -0,0 +1,17 @@
+require_relative "../test_helper"
+
+class UrlTest < Minitest::Spec
+
+  include TestHelpers
+
+  def test_can_list_urls_by_num_times_requested
+    create_payloads
+
+    url_list = Url.sort_url_requests
+
+    assert_equal "http://jumpstartlab.com/", url_list[0][0]
+    assert_equal "http://turing.io/", url_list[1][0]
+    assert_equal "http://yahoo.com/", url_list[2][0]
+  end
+
+end
