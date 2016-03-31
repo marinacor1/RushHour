f6e4d78b41849f922fd4252f225ca56773e04497
marinacor1 <marinaspam1234@gmail.com>
2016-03-30 21:29:01 -0600

    refactoring prts

--- test/models/payload_request_test.rb
+++ test/models/payload_request_test.rb
@@ -100,19 +100,7 @@ class PayloadRequestTest < Minitest::Spec
   end
 
   def test_payload_is_valid 
-    assert PayloadRequest.create({
-                                  url_id: Url.create(address: 'http://jumpstartlab.com/blog').id,
-                                  requested_at: "2013-02-16 21:38:28 -0700",
-                                  responded_in: 37,
-                                  referrer_id: Referrer.create(referred_by:"http://jumpstartlab.com").id,
-                                  request_type_id: RequestType.create(verb: "GET").id,
-                                  event_name: "socialLogin",
-                                  user_id: User.create(browser: UserAgent.parse("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17").browser,
-                                                       os: UserAgent.parse("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17").platform
-                                                       ).id,
-                                  display_id: Display.create(width: "1920", height: "1280").id,
-                                  ip: "63.29.38.211"
-                                }).valid?
+    single_payload.valid?
   end
 
   def test_payload_is_not_valid_if_url_is_missing 
