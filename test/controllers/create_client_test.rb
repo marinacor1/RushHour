bdefda57b3a534ddbb5a2d4c5220a159e32f311a
marinacor1 <marinaspam1234@gmail.com>
2016-03-31 11:37:34 -0600

    first test created

---
+++ test/controllers/create_client_test.rb
@@ -0,0 +1,31 @@
+require './test/test_helper'
+
+class CreateClientTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    RushHour::Server
+  end
+
+  def setup
+    DatabaseCleaner.start
+  end
+
+  def teardown
+    DatabaseCleaner.clean
+  end
+
+  def test_can_create_client_with_valid_attributes
+    assert_equal 0, Client.count
+
+    post '/sources', {client: {identifier: 'jumpstartlab', rootUrl: 'http://jumpstartlab.com' }}
+
+    assert_equal 1, Client.count
+    assert_equal 200, last_response.status
+    assert_equal "jumpstartlab", Client.all.first.identifier
+    assert_equal "http://jumpstartlab.com", Client.all.first.root_url
+  end
+
+
+
+end
