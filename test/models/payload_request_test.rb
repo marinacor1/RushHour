e25d59c3bda135ab084d015a45cbdd91e4aaae4b
marinacor1 <marinaspam1234@gmail.com>
2016-03-30 21:24:34 -0600

    adding single payload

--- test/models/payload_request_test.rb
+++ test/models/payload_request_test.rb
@@ -58,21 +58,22 @@ class PayloadRequestTest < Minitest::Spec
   end
 
   def test_will_return_single_min_and_max_response_time_if_single_payload
-    PayloadRequest.create({ url_id: Url.find_or_create_by(address: "http://jumpstartlab.com/").id,
-                            requested_at: "2013-02-16 21:38:28 -0700",
-                            responded_in: 37,
-                            referrer_id: Referrer.find_or_create_by(referred_by:"http://jumpstartlab.com").id,
-                            request_type_id: RequestType.find_or_create_by(verb: "GET").id,
-                            event_name: "socialLogin",
-                            user_id: User.find_or_create_by(browser: UserAgent.parse("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17").browser,
-                                                 os: UserAgent.parse("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17").platform
-                                                 ).id,
-                            display_id: Display.find_or_create_by(width: "1920", height: "1280").id,
-                            ip: "63.29.38.211"
-                          })
+    single_payload
 
-    assert_equal 37, PayloadRequest.maximum_response_time
-    assert_equal 37, PayloadRequest.minimum_response_time
+    assert_equal 7, PayloadRequest.maximum_response_time
+    assert_equal 7, PayloadRequest.minimum_response_time
+    assert_equal 7, PayloadRequest.average_response_time
+  end
+
+
+  def test_will_report_accurately_when_to_payloads_with_same_response_time_come_in
+    single_payload
+    single_payload
+
+    assert_equal 7, PayloadRequest.maximum_response_time
+    assert_equal 7, PayloadRequest.minimum_response_time
+    assert_equal 7, PayloadRequest.average_response_time
+
 
   end
 
@@ -84,6 +85,36 @@ class PayloadRequestTest < Minitest::Spec
     assert_equal "lastentry", events.last.event_name
     array = "socialLogin", "socialLogin", "socialLogin", "socialLogin", "socialLogin", "socialLogin", "socialLogin", "socialLogin", "socialLogin", "passwordEntry", "passwordEntry", "passwordEntry", "passwordEntry", "passwordEntry", "lastentry"
     assert_equal array, events.map {|event| event.event_name}
+  end
+
+  def test_it_orders_events_in_order_of_frequency_if_same
+    PayloadRequest.create({ url_id: Url.find_or_create_by(address: "http://jumpstartlab.com/").id,
+                            requested_at: "2013-02-16 21:38:28 -0700",
+                            responded_in: 7,
+                            referrer_id: Referrer.find_or_create_by(referred_by:"http://jumpstartlab.com").id,
+                            request_type_id: RequestType.find_or_create_by(verb: "GET").id,
+                            event_name: "socialLogin",
+                            user_id: User.find_or_create_by(browser: UserAgent.parse("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17").browser,
+                                                 os: UserAgent.parse("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17").platform
+                                                 ).id,
+                            display_id: Display.find_or_create_by(width: "1920", height: "1280").id,
+                            ip: "63.29.38.211"
+                          })
+
+    PayloadRequest.create({ url_id: Url.find_or_create_by(address: "http://jumpstartlab.com/").id,
+                            requested_at: "2013-02-16 21:38:28 -0700",
+                            responded_in: 7,
+                            referrer_id: Referrer.find_or_create_by(referred_by:"http://jumpstartlab.com").id,
+                            request_type_id: RequestType.find_or_create_by(verb: "GET").id,
+                            event_name: "socialLogin",
+                            user_id: User.find_or_create_by(browser: UserAgent.parse("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17").browser,
+                                                 os: UserAgent.parse("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17").platform
+                                                 ).id,
+                            display_id: Display.find_or_create_by(width: "1920", height: "1280").id,
+                            ip: "63.29.38.211"
+                          })
+
+
   end
 
   def test_payload_is_valid 
