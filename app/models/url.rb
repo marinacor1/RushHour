16b31f528f87f5c6d03825b8782dd6d4789ac57d
Ashwin Rao <theonlyrao@gmail.com>
2016-04-01 09:21:27 -0600

    finished cleaning up url class

--- app/models/url.rb
+++ app/models/url.rb
@@ -6,45 +6,6 @@ class Url < ActiveRecord::Base
   has_many :referrers, through: :payload_requests
   has_many :users, through: :payload_requests
 
-  def self.payloads_of(url)
-    if url == "all"
-      PayloadRequest.all
-    else
-      PayloadRequest.where(url_id: url.id)
-    end
-  end
-
-  def self.group_by_count_of(url, attribute)
-    self.payloads_of(url).group(attribute).count.sort_by do |attribute, count|
-      count
-    end.reverse
-  end
-
-  def self.get_result_from(attribute_ids, attribute)
-    attribute_ids.map do |id_pair|
-      if attribute == "referred_by"
-        Referrer.where(id: id_pair[0]).first.referred_by
-      elsif attribute == "browser"
-        User.where(id: id_pair[0]).first.browser
-      elsif attribute == "os"
-        User.where(id: id_pair[0]).first.os
-      elsif attribute == "address"
-        Url.where(id: id_pair[0]).first.address
-      end
-    end
-  end
-
-  def self.sort_url_requests
-    # sorted_ids = self.group_by_count_of("all", :url_id)
-    # most_urls = self.get_result_from(sorted_ids, "address")
-    binding.pry
-
-  end
-
-  # def self.max_response_time(url)
-  #   self.payloads_of(url).maximum(:responded_in)
-  # end
-
   def max_response_time
     payload_requests.maximum(:responded_in)
   end
@@ -79,9 +40,6 @@ class Url < ActiveRecord::Base
   end
 
   def popular_user_agents
-    #TODO this would be worth looking at and seeing
-    #if our test output was wrong initially or if it's wrong now
-
     popular = payload_requests.group(:user_id).count
     sorted = popular.sort_by do |user_id, count|
       count
@@ -89,14 +47,5 @@ class Url < ActiveRecord::Base
     sorted.map do |user_id, count|
       User.where(id: user_id).pluck(:os, :browser)
     end[0..2]
-    # grouped_systems = users.group(:os).count
-    # grouped_browsers = users.group(:browser).count
-    # (grouped_systems.keys).zip(grouped_browsers.keys)
-    # binding.pry
-    # binding.pry
-    # sorted_ids = self.group_by_count_of(url, :user_id)
-    # browsers = self.get_result_from(sorted_ids, "browser")
-    # systems = self.get_result_from(sorted_ids, "os")
-    # systems.zip(browsers)[0..2]
   end
 end
