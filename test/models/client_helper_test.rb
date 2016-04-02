7eac2fe9a2726345178507ec7c7ab1e1b1b0344f
patrickwhardy <patrickwhardy@gmail.com>
2016-04-02 12:48:23 -0600

    client helper params fixed

--- test/models/client_helper_test.rb
+++ test/models/client_helper_test.rb
@@ -4,14 +4,14 @@ class ClientHelperTest < Minitest::Spec
   include TestHelpers
 
   def test_can_create_multiple_clients_with_valid_attributes
-    params = {"identifier": "jumpstartlab", "rootUrl": "http://jumpstartlab.com"}
+    params = {"identifier"=>"jumpstartlab", "rootUrl"=>"http://jumpstartlab.com"}
     client_helper = ClientHelper.new(params)
 
     assert_equal 200, client_helper.returned[0]
     assert_equal "identifier: jumpstartlab", client_helper.returned[1]
     assert_equal 1, Client.all.count
 
-    params = {"identifier": "google", "rootUrl": "http://google.com"}
+    params = {"identifier"=>"google", "rootUrl"=>"http://google.com"}
     client_helper = ClientHelper.new(params)
 
     assert_equal 200, client_helper.returned[0]
@@ -20,12 +20,12 @@ class ClientHelperTest < Minitest::Spec
   end
 
   def test_403_if_client_already_exists
-    params = {"identifier": "jumpstartlab", "rootUrl": "http://jumpstartlab.com"}
+    params = {"identifier"=>"jumpstartlab", "rootUrl"=>"http://jumpstartlab.com"}
     client_helper = ClientHelper.new(params)
 
     assert_equal 1, Client.all.count
 
-    params = {"identifier": "jumpstartlab", "rootUrl": "http://jumpstartlab.com"}
+    params = {"identifier"=>"jumpstartlab", "rootUrl"=>"http://jumpstartlab.com"}
     client_helper = ClientHelper.new(params)
 
     assert_equal 403, client_helper.returned[0]
@@ -34,14 +34,14 @@ class ClientHelperTest < Minitest::Spec
   end
 
   def test_400_if_invalid_attributes
-    no_id = {"rootUrl": "http://jumpstartlab.com"}
+    no_id = {"rootUrl"=>"http://jumpstartlab.com"}
     client_helper_id = ClientHelper.new(no_id)
 
     assert_equal 400, client_helper_id.returned[0]
     assert_equal "Identifier can't be blank", client_helper_id.returned[1]
     assert_equal 0, Client.all.count
 
-    no_url = {"identifier": "jumpstartlab"}
+    no_url = {"identifier"=>"jumpstartlab"}
     client_helper_url = ClientHelper.new(no_url)
 
     assert_equal 400, client_helper_url.returned[0]
