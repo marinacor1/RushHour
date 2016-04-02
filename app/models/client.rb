49a91559dc7f779d4869f066b905d3bace176bd4
patrickwhardy <patrickwhardy@gmail.com>
2016-04-02 11:01:44 -0600

    fixing client issue with urls

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,7 @@
 
 class Client < ActiveRecord::Base
   has_many :payload_requests
+  has_many :urls, through: :payload_requests
   validates :identifier, presence: true
   validates :identifier, uniqueness: true
   validates :root_url, presence: true
@@ -35,13 +36,13 @@ class Client < ActiveRecord::Base
   end
 
   def most_popular_urls
-    popular = payload_requests.group(:user_id).count
+    popular = payload_requests.group(:url_id).count
     sorted = popular.sort_by do |url_id, count|
       count
     end.reverse
     sorted.map do |url_id, count|
       Url.where(id: url_id).pluck(:address)
-    end
+    end.flatten
   end
 
   def browser_breakdown
--- test/features/user_can_see_client_analytics.rb
+++ test/features/user_can_see_client_analytics.rb
@@ -35,4 +35,31 @@ class UserCanSeeURLAnalytics < Minitest::Test
      assert page.has_content? 37
   end
 
+  def test_user_can_see_data_for_multiple_payloads
+    post '/sources', {identifier: 'jumpstartlab', rootUrl: 'http://jumpstartlab.com' }
+    assert_equal "jumpstartlab", Client.all.first.identifier
+
+    post '/sources/jumpstartlab/data',
+    {"payload"=>
+      "{\"url\":\"http://jumpstartlab.com/article\",\"requestedAt\":\"2013-02-16 02:38:28 -0700\",\"respondedIn\":55,\"referredBy\":\"http://google.com\",\"requestType\":\"POST\",\"parameters\":[],\"eventName\":\"search\",\"userAgent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",\"resolutionWidth\":\"1000\",\"resolutionHeight\":\"800\",\"ip\":\"64.29.38.211\"}",
+      "splat"=>[],
+      "captures"=>["jumpstartlab"],
+      "id"=>"jumpstartlab"}
+
+    post '/sources/jumpstartlab/data',
+    {"payload"=>
+      "{\"url\":\"http://jumpstartlab.com/blog\",\"requestedAt\":\"2013-02-16 21:38:28 -0700\",\"respondedIn\":37,\"referredBy\":\"http://jumpstartlab.com\",\"requestType\":\"GET\",\"parameters\":[],\"eventName\":\"socialLogin\",\"userAgent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",\"resolutionWidth\":\"1920\",\"resolutionHeight\":\"1280\",\"ip\":\"63.29.38.211\"}",
+     "splat"=>[],
+     "captures"=>["jumpstartlab"],
+     "id"=>"jumpstartlab"}
+    #  client = Client.all.last
+     visit "/sources/jumpstartlab"
+     save_and_open_page
+     assert page.has_content? "jumpstartlab"
+     assert page.has_content? "GET"
+     assert page.has_content? 37
+     assert page.has_content? "jumpstartlab.com/blog"
+     assert page.has_content? "jumpstartlab.com/article"
+  end
+
 end
