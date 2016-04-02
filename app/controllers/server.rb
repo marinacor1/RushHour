01a7481e3832bcd16b387540a1039fee0f425eda
Ashwin Rao <theonlyrao@gmail.com>
2016-04-01 23:39:33 -0600

    refactoring into client helper done

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -6,21 +6,8 @@ module RushHour
     end
 
     post '/sources' do
-      client = Client.new(identifier: params[:identifier], root_url: params[:rootUrl] )
-      if client.save
-        status 200
-        body "identifier: #{client.identifier}"
-      elsif Client.find_by(:identifier == params[:identifier])
-        status 403
-        body client.errors.full_messages.join(", ")
-      else
-        status 400
-        body client.errors.full_messages.join(", ")
-      end
-      # returned_info = [200, "The body"]
-      # client_parser = ClientParser.new(params)
-      #
-      # status, body = client_parser
+      client = ClientHelper.new(params)
+      status, body = client.returned
     end
 
     post '/sources/:id/data' do |id|
