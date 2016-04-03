c3e3f3c411cfd0a0065191d81acc448699e4de7a
marinacor1 <marinaspam1234@gmail.com>
2016-04-02 18:50:21 -0600

    url works

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -368,5 +368,18 @@ module TestHelpers
                             param: "6"
                           })
 
+    PayloadRequest.create({ url_id: Url.find_or_create_by(address: "http://turing.io/").id,
+                            requested_at: "2013-02-16 21:38:28 -0700",
+                            responded_in: 7,
+                            referrer_id: Referrer.find_or_create_by(referred_by:"http://chacha.com").id,
+                            request_type_id: RequestType.find_or_create_by(verb: "GET").id,
+                            event_name: "socialLogin",
+                            user_id: User.find_or_create_by(browser: UserAgent.parse("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17").browser,
+                                                 os: UserAgent.parse("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17").platform
+                                                 ).id,
+                            display_id: Display.find_or_create_by(width: "1920", height: "1280").id,
+                            ip: "63.29.38.211",
+                            param: "7"
+                          })
   end
 end
