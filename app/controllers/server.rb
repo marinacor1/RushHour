03b12d4674a55c5260f868c4890cb4b3248821f1
Ashwin Rao <theonlyrao@gmail.com>
2016-04-02 23:05:59 -0600

    cleaned up

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -41,24 +41,16 @@ module RushHour
     end
 
     get '/sources/:id/events/:event' do |id, event|
-      # client = find_client_from_url(id)
       payloads = event_payloads(id, event)
       if payloads.empty?
         @identifier = id
         erb :event_error
       else
         @total = payloads.count
-        # hour_collection = payloads.map do |payload|
-        #   payload.param.split[1].split(":")[0]
-        # end
-        # @hour_count = hour_collection.group_by {|h| h}
-        # @hour_count.map {|k,v| @hour_count[k] = v.count}
-        # @hour_count.default = "0"
         @hour_count = parse_times_of(payloads)
         @event = event
         erb :show_event
       end
     end
-
   end
 end
