7c959ca9304731080b15ba739bb4bb14b3dc4455
Ashwin Rao <theonlyrao@gmail.com>
2016-04-04 08:55:40 -0600

    fixed test using marina's code

--- test/models/url_test.rb
+++ test/models/url_test.rb
@@ -74,6 +74,34 @@ class UrlTest < Minitest::Spec
 
   def test_can_find_three_most_popular_referrers
     create_payloads
+    PayloadRequest.create({ url_id: Url.find_or_create_by(address: "http://jumpstartlab.com/").id,
+                            requested_at: "2013-02-16 21:38:28 -0700",
+                            responded_in: 37,
+                            referrer_id: Referrer.find_or_create_by(referred_by:"http://bing.com").id,
+                            request_type_id: RequestType.find_or_create_by(verb: "GET").id,
+                            event_name: "socialLogin",
+                            user_id: User.find_or_create_by(browser: UserAgent.parse("Safari/537.17").browser,
+                                                 os: UserAgent.parse("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17").platform
+                                                 ).id,
+                            display_id: Display.find_or_create_by(width: "1920", height: "1280").id,
+                            ip: "63.29.38.211",
+                            client_id: 1,
+                            param: "145"
+                          })
+    PayloadRequest.create({ url_id: Url.find_or_create_by(address: "http://jumpstartlab.com/").id,
+                            requested_at: "2013-02-16 21:38:28 -0700",
+                            responded_in: 37,
+                            referrer_id: Referrer.find_or_create_by(referred_by:"http://google.com").id,
+                            request_type_id: RequestType.find_or_create_by(verb: "GET").id,
+                            event_name: "socialLogin",
+                            user_id: User.find_or_create_by(browser: UserAgent.parse("Safari/537.17").browser,
+                                                 os: UserAgent.parse("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17").platform
+                                                 ).id,
+                            display_id: Display.find_or_create_by(width: "1920", height: "1280").id,
+                            ip: "63.29.38.211",
+                            client_id: 1,
+                            param: "144"
+                          })
 
     url = Url.find_by(address: "http://jumpstartlab.com/")
 
