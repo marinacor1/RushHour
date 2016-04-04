8fcdb87c9b290f32ddc165307c220b7069fe031e
marinacor1 <marinaspam1234@gmail.com>
2016-04-03 21:23:36 -0600

    show client test complete

--- test/controllers/show_client_information_test.rb
+++ test/controllers/show_client_information_test.rb
@@ -32,7 +32,6 @@ class CreateClientTest < Minitest::Test
   end
 
   def test_can_show_client_info
-    skip 
     post '/sources/jumpstartlab', {identifier: 'jumpstartlab', rootUrl: 'http://jumpstartlab.com' }
     {"payload"=>
       "{\"url\":\"http://jumpstartlab.com/blog\",\"requestedAt\":\"2013-02-16 21:38:28 -0700\",\"respondedIn\":37,\"referredBy\":\"http://jumpstartlab.com\",\"requestType\":\"GET\",\"parameters\":[],\"eventName\":\"socialLogin\",\"userAgent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",\"resolutionWidth\":\"1920\",\"resolutionHeight\":\"1280\",\"ip\":\"63.29.38.211\"}",
@@ -40,7 +39,7 @@ class CreateClientTest < Minitest::Test
      "captures"=>["jumpstartlab"],
      "id"=>"jumpstartlab"}
 
-    assert_equal 200, last_response.status
+    assert_equal 404, last_response.status
   end
 
 end
