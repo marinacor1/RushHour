0fb9b224ca533d28fe5296937a54d0a47ff444b6
marinacor1 <marinaspam1234@gmail.com>
2016-04-03 21:09:08 -0600

    403 test written

--- test/models/payload_helper_test.rb
+++ test/models/payload_helper_test.rb
@@ -59,7 +59,20 @@ class PayloadHelperTest < Minitest::Spec
 
     assert_equal 400, helper.returned[0]
     assert_equal "Url can't be blank ,Requested at can't be blank ,Responded in can't be blank ,Referrer can't be blank ,Request type can't be blank ,Event name can't be blank ,User can't be blank ,Display can't be blank ,Ip can't be blank ,Param can't be blank", helper.returned[1]
-
-
   end
+
+  def test_can_return_403_when_client_does_not_exist
+     params = {"payload"=>
+      "{\"url\":\"http://jumpstartlab.com/blog\",\"requestedAt\":\"2013-02-16 21:38:28 -0700\",\"respondedIn\":37,\"referredBy\":\"http://jumpstartlab.com\",\"requestType\":\"GET\",\"parameters\":[],\"eventName\":\"socialLogin\",\"userAgent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",\"resolutionWidth\":\"1920\",\"resolutionHeight\":\"1280\",\"ip\":\"63.29.38.211\"}",
+     "splat"=>[],
+     "captures"=>["jumpstartlab"],
+     "id"=>"jumpstartlab"}
+     id = "jumpstartlab"
+     Client.create(identifier: 'mtv', root_url: 'http://mtv.com')
+     helper = PayloadHelper.new(params)
+
+    assert_equal 403, helper.returned[0]
+    assert_equal "Client does not exist", helper.returned[1]
+  end
+
 end
